221a7f3c29d0b42ad0bb3cb8b268805023fc08af
Jessica Brisson <jbrisson2@studentmail.nait.ca>
2016-10-04 16:03:37 -0600

    add eRestaurant DB context classes, and table classes

---
+++ eRestaurantSystem.Data/Entities/MenuCategory.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#region 
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+#endregion
+
+namespace eRestaurantSystem.Data.Entities
+{   [Table("MenuCategories")]
+    public class MenuCategory
+    {
+        [Key]
+        public int MenuCategoryID { get; set; }
+        public string Description { get; set; }
+
+        public virtual ICollection<Item> Items { get; set; }
+    }
+}
